8003f255d33bf6eae0dd86c54e0739c1c3232d07
Mina-Yun <dbsalsk222@gmail.com>
2024-08-05 10:00:39 +0900

    [fix] 고침

--- lib/model/imgtest.dart
+++ lib/model/imgtest.dart
@@ -1,5 +1,4 @@
 import 'dart:convert';
-import 'dart:io';
 import 'dart:typed_data';
 
 import 'package:flutter/material.dart';
@@ -24,26 +23,57 @@ class _albumState extends State<album> {
 
   Future<void> _loadImage() async {
     try {
-      var img =
-      await _webSocketService.transmit({'id': 'rlawndud'}, 'GetAllImage');
+      var img = await _webSocketService.transmit({'id': 'rlawndud'}, 'GetAllImage');
+      debugPrint('이미지 로드 응답 : $img');
 
-      debugPrint(img.toString());
-      images.add(Picture.fromJson(img));
+      if (img != null && img['img_data'] != null) {
+        setState(() {
+          images.add(Picture.fromJson(img));
+        });
+      } else {
+        debugPrint('이미지 로드 실패');
+      }
     } catch (e) {
-      e.printError();
+      debugPrint('이미지 로딩 에러 : $e');
     }
   }
 
   void _printImage() {
     if (images.isNotEmpty) {
       print('images의 개수:${images.length}');
-      for (var img in images) {
-        print('$img');
-        pic.add(BytesToImage(img.img_data));
-
+      setState(() {
+        for (var img in images) {
+          print('$img');
+          pic.add(BytesToImage(img.img_data));
+        }
         print('pic의 개수:${pic.length}');
-        setState(() {});
+      });
+    }
+  }
+
+  Future<void> _takePictureAndSend() async {
+    try {
+      final ImagePicker picker = ImagePicker();
+      final XFile? photo = await picker.pickImage(source: ImageSource.camera);
+
+      if (photo != null) {
+        Uint8List imgBytes = await photo.readAsBytes();
+        debugPrint('이미지를 바이트로 읽음');
+        var response = await _webSocketService.transmit({'image': imgBytes}, 'SendImage');
+        debugPrint('서버 응답 : $response');
+
+        if (response != null && response['success'] == true) {
+          // 서버로부터 이미지를 받았다는 가정하에 처리
+          setState(() {
+            images.add(Picture.fromJson(response));
+            pic.add(BytesToImage(response['img_data']));
+          });
+        } else {
+          debugPrint('서버로 전달 실패');
+        }
       }
+    } catch (e) {
+      debugPrint('서버로 사진을 보내는동안 오류가 발생: $e');
     }
   }
 
@@ -68,28 +98,32 @@ class _albumState extends State<album> {
             style: TextButton.styleFrom(
                 backgroundColor: mainColor30, foregroundColor: Colors.white),
           ),
+          TextButton(
+            child: Text('사진 촬영 및 전송'),
+            onPressed: _takePictureAndSend,
+            style: TextButton.styleFrom(
+                backgroundColor: mainColor30, foregroundColor: Colors.white),
+          ),
           Expanded(
             child: ListView.builder(
               itemCount: pic.length,
               itemBuilder: (BuildContext context, int index) {
                 return SizedBox(
                   height: 200,
-                  child: Image.memory(pic[0]),
+                  child: Image.memory(pic[index]),
                 );
               },
             ),
           ),
-          /*Container(
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(5),
-              image: DecorationImage(
-                fit: BoxFit.cover,
-                image: FileImage(File(images[index]!.path)),
-              ),
-            ),
-          ),*/
         ],
       ),
     );
   }
+}
+
+// BytesToImage 함수가 정의되어 있는지 확인해야 합니다.
+Uint8List BytesToImage(dynamic imgData) {
+  // imgData를 Uint8List로 변환하는 로직이 필요합니다.
+  // 예시:
+  return Uint8List.fromList(List<int>.from(imgData));
 }
